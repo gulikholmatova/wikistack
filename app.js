290c7f97daa9dfb0a8b4d3861c2ca5d11cba58ce
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-09 12:00:30 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+})
