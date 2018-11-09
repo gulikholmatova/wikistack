fa5d57bfac0da840c9c709e42741fe4200406974
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-09 12:42:42 -0500

    Commit

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
+const { db } = require('./models');
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+  },
+  slug: {
+    type: Sequelize.STRING,
+  },
+  content: {
+    type: Sequelize.TEXT,
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+});
+
+module.exports = { Page, User };
 
 const PORT = 3000;
 app.listen(PORT, () => {
