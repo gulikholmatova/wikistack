c2ec5394d3e4e40aa0ea5cbdb915bdc84e43a92d
jentcain <jentcain@gmail.com>
2018-11-09 12:25:26 -0500

    installed so much, added a listen

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
-})
+  res.send("Hello world!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
